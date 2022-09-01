364d3362d3bbbe2079a82e75d6a7ce1bb109c6f9
许大仙 <1900919313@qq.com>
2022-09-01 11:55:18 +0800

    ✨ 支持 ES5

--- vite-vue3-jsx/vite.config.js
+++ vite-vue3-jsx/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
   ],
   resolve: {
     alias: [
