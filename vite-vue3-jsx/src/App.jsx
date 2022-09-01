6ce7359778c3be56d0ca252ecb69d69185a85a54
许大仙 <1900919313@qq.com>
2022-09-01 10:36:34 +0800

    ✨

--- vite-vue3-jsx/src/App.jsx
+++ vite-vue3-jsx/src/App.jsx
@@ -1,7 +1,14 @@
 import { defineComponent } from 'vue'
+import '@/styles/hello.css'
+
+const url = new URL('./assets/vue.svg', import.meta.url)
+
 const Test = defineComponent({
     render() {
-        return (<div>你好啊，Vite jsx </div>)
+        return (<div class="title">
+            <img src={ url.href} alt="" />
+            你好啊，Vite jsx
+        </div>)
     }
 })
 export { Test }
